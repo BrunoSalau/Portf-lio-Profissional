f227f7ceabfd0fc0ff33a99452d21a4e56d4b1cd
BrunoSalau <brunosalau@gmail.com>
2025-10-21 22:30:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let projetos = document.getElementById('projetos')
+let curriculo = document.getElementById('curriculo')
+
+function subir1(){
+    projetos.style.transform = "translateY(-10px)"
+}
+function descer1(){
+    projetos.style.transform = "translateY(0px)"
+}
+function subir2(){
+    curriculo.style.transform = "translateY(-10px)"
+}
+function descer2(){
+    curriculo.style.transform = "translateY(0px)"
+}
